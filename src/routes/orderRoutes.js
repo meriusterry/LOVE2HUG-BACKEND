0452e9355520c5d3ccc68d05b4d5676ffd92a7f2
Terry Ntuli <meriusterry>
2026-05-05 09:20:49 +0200

    second  commit with some new fixes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -14,6 +14,7 @@ const { protect, admin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', protect, createOrder);
+
 router.get('/', protect, getOrders);
 router.get('/stats', protect, admin, getOrderStats);
 router.get('/payment-status/:orderNumber', protect, getPaymentStatus); // For frontend polling
